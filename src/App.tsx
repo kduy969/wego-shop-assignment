6b90bc91fbb7b1e459c766e6e645f09fd5ed7984
kduy969 <kduyit92@gmail.com>
2023-07-04 14:26:59 +0700

    Refactor app setup logic, remove unused files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import "./App.css";
-import "@material-design-icons/font";
 import Shop from "./ui/shop/shop";
-import { Service } from "./service";
 import React from "react";
-Service.init();
 
 function App() {
   return <Shop />;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import "@material-design-icons/font";
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import setup from "./setup";
+setup();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
