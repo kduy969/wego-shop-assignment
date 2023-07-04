51f64e3ebb72aa1e9756a924b732086117af4b14
kduy969 <kduyit92@gmail.com>
2023-07-04 21:41:07 +0700

    Prevent scroll to bottom reoccur on callback change.

--- src/hooks/useScrollToBottom.ts
+++ src/hooks/useScrollToBottom.ts
@@ -1,21 +1,22 @@
 import * as React from "react";
+import { useRef } from "react";
 
 export const useScrollToBottom = <T extends Element>(
   onHitBottom: () => void
 ): [React.RefCallback<T>] => {
   const [node, setRef] = React.useState<any>(null);
+  const isBottomRef = useRef(false);
 
   React.useEffect(() => {
     let observer: IntersectionObserver;
-    let isBottom = false;
 
     if (node && node.parentElement) {
       observer = new IntersectionObserver(
         ([entry]) => {
           const newIsBottom = entry.isIntersecting;
-          if (newIsBottom !== isBottom) {
-            isBottom = newIsBottom;
-            if (isBottom) {
+          if (newIsBottom !== isBottomRef.current) {
+            isBottomRef.current = newIsBottom;
+            if (isBottomRef.current) {
               onHitBottom();
             }
           }
